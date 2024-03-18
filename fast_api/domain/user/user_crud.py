3ac6640e3fa761a0837cf7d7b001b0fe8affd533
chminii <chminipark@gmail.com>
2024-03-18 15:30:57 +0900

    [Feat] : user_create 중복검사, 에러핸들링
    Git : #1

--- fast_api/domain/user/user_crud.py
+++ fast_api/domain/user/user_crud.py
@@ -2,18 +2,19 @@ from sqlalchemy.orm import Session
 from domain.user.user_schema import UserCreate
 from database.models import User
 import bcrypt
+from typing import Optional
 
 def create_user(db: Session, user_create: UserCreate):
     db_user = User(
-        email = user_create.email,
-        password = hash_password(user_create.password1),
-        )
+        email=user_create.email,
+        password=hash_password(user_create.password1),
+    )
     db.add(db_user)
     db.commit()
 
 # Hash a password using bcrypt
 def hash_password(password: str) -> bytes:
-    pwd_bytes = password.encode('utf-8')
+    pwd_bytes = password.encode("utf-8")
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt)
     return hashed_password
@@ -22,3 +23,7 @@ def hash_password(password: str) -> bytes:
 # def verify_password(plain_password, hashed_password):
 #     password_byte_enc = plain_password.encode('utf-8')
 #     return bcrypt.checkpw(password = password_byte_enc , hashed_password = hashed_password)
+
+def get_existing_user(db: Session, user_create: UserCreate) -> Optional[User]:
+    user = db.query(User).filter(User.email == user_create.email).first()
+    return user
