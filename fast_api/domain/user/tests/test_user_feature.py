0d4764a4590d79ff8c16e9fb05fcb292c5d46f25
chminii <chminipark@gmail.com>
2024-04-23 20:51:41 +0900

    [Refactor] : Path 관련 코드 수정
    Git : #1

---
+++ fast_api/domain/user/tests/test_user_feature.py
@@ -0,0 +1,32 @@
+# from ..features import user_feature
+import sys
+# from pathlib import Path
+# _path = Path().parent.joinpath('/features')
+# sys.path.append(_path)
+
+
+# from ..features import user_feature
+
+
+# def test_test():
+#     print('feafeaf')
+
+#     # print(path.as_uri)
+
+#     assert 1 == 1
+
+
+# if __name__ == '__main__':
+#     from pathlib import Path
+#     here = Path(__file__).parent
+#     features = here / "/features"
+#     sys.path.append(features)
+
+#     print(features.as_uri)
+
+
+    # import sys
+    # from pathlib import Path
+    # path = Path().parent.joinpath('/features')
+    # print(path.as_uri)
+    # sys.path.append()
--- fast_api/main.py
+++ fast_api/main.py
@@ -1,23 +1,34 @@
 from typing import Optional
 from fastapi import FastAPI
 
+# from database.database import engine
+# from database import models
+# models.Base.metadata.create_all(bind=engine)
+
+# Path
+from sys import path as sys_path
+from pathlib import Path
+sys_path.append(Path(__file__))
 from database.database import engine
 from database import models
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-from domain.user import user_routers
-app.include_router(user_routers.router)
+# from domain.user import user_routers
+# app.include_router(user_routers.router)
+
+hello_world = 'Hellow World!'
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return hello_world
 
+# if __name__ != '__main__':
+#     hello_world = __name__
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {
-        "item_id": item_id,
-        "q": q,
-    }
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {
+#         "item_id": item_id,
+#         "q": q,
+#     }
