5d50cd3c1b600d361dd3806ed673b40d194ea658
chminii <chminipark@gmail.com>
2024-06-02 20:17:02 +0900

    [Fix] : update eb_fast_api_dev uvicorn port number

--- api/eb_fast_api/main.py
+++ api/eb_fast_api/main.py
@@ -42,4 +42,4 @@ def read_root():
     return 'Hellow World!'
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=8001, reload=True)
