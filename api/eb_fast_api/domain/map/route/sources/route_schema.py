f0ba2093d2ebd2f4c52fb2896ec8ce0d4ef116b8
Ubuntu <ubuntu@ip-172-31-46-53.ap-northeast-2.compute.internal>
2024-07-05 02:38:19 +0000

    [Refactor] : Transportation 모델 리팩토링
    Git : #12

--- api/eb_fast_api/domain/map/route/sources/route_schema.py
+++ api/eb_fast_api/domain/map/route/sources/route_schema.py
@@ -1,87 +1,8 @@
-# from pydantic import BaseModel
-# from typing import List, Optional, Self
-
-# def getDataFromJson(k: str, j: dict) -> Optional[any]:
-#     try:
-#         return j[k]
-#     except:
-#         return None
-    
-# class Lane(BaseModel):
-#     subwayName: Optional[str] = None
-#     busName: Optional[str] = None
-
-#     @classmethod
-#     def fromJson(cls, j: dict) -> Self:
-#         subwayName = getDataFromJson('name', j)
-#         busName = getDataFromJson('busNo', j)
-#         lane = Lane(
-#             subwayName = subwayName,
-#             busName = busName
-#             )
-#         return lane
-
-
-# class SubPath(BaseModel):
-#     trafficType: int
-#     sectionTime: int
-#     lanes: Optional[List[Lane]]
-
-#     @classmethod
-#     def fromJson(cls, j: dict) -> Self:
-#         tmpLanes = getDataFromJson('lane', j)
-#         lanes = list(map(Lane.fromJson, tmpLanes)) if tmpLanes != None else None
-#         subPath = SubPath(
-#             trafficType = j['trafficType'],
-#             sectionTime = j['sectionTime'],
-#             lanes = lanes
-#         )
-#         return subPath
-
-
-# class PathInfo(BaseModel):
-#     totalTime: int
-#     totalWalkTime: int = 0
-#     payment: int
-#     busTransitCount: int
-#     subwayTransitCount: int
-
-
-# class Path(BaseModel):
-#     pathType: int
-#     info: PathInfo
-#     subPaths: List[SubPath]
-
-#     @classmethod
-#     def fromJson(cls, j: dict) -> Self:
-#         info = PathInfo.model_validate(j['info'])
-#         subPaths = list(map(SubPath.fromJson, j['subPath']))
-#         path = Path(
-#             pathType = j['pathType'],
-#             info = info,
-#             subPaths = subPaths
-#         )
-#         return path
-
-
-# class Route(BaseModel):
-#     searchType: int
-#     paths: List[Path]
-
-#     @classmethod
-#     def fromJson(cls, j: dict) -> Self:
-#         paths = list(map(Path.fromJson, j['path']))
-#         route = Route(
-#             searchType = j['searchType'],
-#             paths = paths
-#         )
-#         return route
-
 from pydantic import BaseModel
 from typing import List, Optional, Self
 from eb_fast_api.snippets import snippets
     
-class Transportation(BaseModel):
+class Transport(BaseModel):
     subwayName: Optional[str] = None
     busName: Optional[str] = None
 
@@ -89,7 +10,7 @@ class Transportation(BaseModel):
     def fromJson(cls, j: dict) -> Self:
         subwayName = snippets.getDataFromJson('name', j)
         busName = snippets.getDataFromJson('busNo', j)
-        trans = Transportation(
+        trans = Transport(
             subwayName = subwayName,
             busName = busName
             )
@@ -98,16 +19,16 @@ class Transportation(BaseModel):
 class SubPath(BaseModel):
     type: int
     time: int
-    transportations: Optional[List[Transportation]]
+    transports: Optional[List[Transport]]
 
     @classmethod
     def fromJson(cls, j: dict) -> Self:
         tmpTrans = snippets.getDataFromJson('lane', j)
-        transportations = list(map(Transportation.fromJson, tmpTrans)) if tmpTrans != None else None
+        transports = list(map(Transport.fromJson, tmpTrans)) if tmpTrans != None else None
         subPath = SubPath(
             type = j['trafficType'],
             time = j['sectionTime'],
-            transportations = transportations
+            transports = transports
         )
         return subPath
     
