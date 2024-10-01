c7b4765c20e4c1493a03fdba3ef489c70f3f8675
Ubuntu <ubuntu@ip-172-31-0-134.ap-northeast-2.compute.internal>
2024-10-01 10:39:53 +0000

    [Fix] : database init 수정
    Git : #25

--- eb_fast_api/env/sources/env.py
+++ eb_fast_api/env/sources/env.py
@@ -39,6 +39,7 @@ def MYSQL_VALUE(env_file: Path):
 
 def TEST_USER_VALUE(env_file: Path):
     class TEST_USER_BaseSettings(BaseSettings):
+        name: str
         email: str
         password: str
 
